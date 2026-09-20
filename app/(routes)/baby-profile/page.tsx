a97cb21cd81612341cb4f5434f3771f200a5efad
buzznation-manish <manish@buzznationmarketing.com>
2026-09-20 23:50:03 +0530

    Add protected BabyProfile page

---
+++ app/(routes)/baby-profile/page.tsx
@@ -0,0 +1,9 @@
+import BabyProfileForm from '@/components/baby-profile-form';
+
+export default function BabyProfilePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-amber-50 via-rose-50 to-sky-50 px-6 py-12">
+      <BabyProfileForm />
+    </main>
+  );
+}
